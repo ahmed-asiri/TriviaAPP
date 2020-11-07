474589d6175d7829f2bdeccf4ace5ca2c56832e6
Ahmed Asiri <ia7mad13@gmail.com>
2020-11-08 00:21:54 +0300

    Fixing the problem if integrating FEND and BEND

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -13,7 +13,7 @@ def pagination(request, questions_list):
     page = request.args.get('page', 1, type=int)
     start = (page - 1) * QUESTIONS_PER_PAGE
     end = start + QUESTIONS_PER_PAGE
-    formatted_questions = [question.format() for question in questions_list]
+    formatted_questions = [question.format() for question in questions_list[start:end]]
     return formatted_questions
 
 
@@ -47,18 +47,17 @@ def create_app(test_config=None):
   @app.route('/categories')
   def get_categories():
     
-    categories = Category.query.order_by(Category.id).all()
+    categories = Category.query.order_by(Category.type).all()
     
     # check the len, if no categories exist, abort the request and handle it.
     if len(categories) == 0:
       abort(404)
 
-    # format the categories in proper way for the front-end.
-    formatted_categories = [category.format() for category in categories]
+
 
     return jsonify({
       "success": True,
-      "categories": categories
+      "categories": {category.id: category.type for category in categories}
     })
 
 
@@ -79,7 +78,6 @@ def create_app(test_config=None):
   def get_questions():
     questions = Question.query.all()
     paged_questions = pagination(request, questions)
-
     if(len(paged_questions) == 0):
       abort(404)
     
@@ -90,7 +88,8 @@ def create_app(test_config=None):
       "success": True,
       "questions": paged_questions,
       "total_questions": len(questions),
-      "categories": category_dict
+      "categories": category_dict,
+      "current_category": None
     }
 
   
@@ -140,10 +139,6 @@ def create_app(test_config=None):
   '''
 
   '''
-  I HAVE COMBINED BOTH OF THESES ENDPOINTS INTO ONE
-  '''
-  
-  '''
   @TODO: 
   Create a POST endpoint to get questions based on a search term. 
   It should return any questions for whom the search term 
@@ -154,16 +149,19 @@ def create_app(test_config=None):
   Try using the word "title" to start. 
   '''
   
-
+  '''
+  I HAVE COMBINED BOTH OF THESES ENDPOINTS INTO ONE
+  '''
+  
   @app.route('/questions', methods=['POST'])
   def create_question():
 
     
+    body = request.get_json()
+    search_term = body.get('searchTerm', None)
 
-    search_term = request.args.get("searchTerm")
     if search_term:
       # search for question
-      
       questions = Question.query.filter(
         Question.question.ilike(f'%{search_term}%')).all()
 
@@ -174,17 +172,17 @@ def create_app(test_config=None):
       return jsonify({
         "success": True,
         "questions": formatted_questions,
-        "total_questions": len(questions)
+        "total_questions": len(questions),
+        "current_category": None
       }), 200
 
     else:
-      # create question
-      question_data = request.get_json()
 
-      question_text = question_data.get('question', None)
-      answer = question_data.get('answer', None)
-      category = question_data.get('category', None)
-      difficulty = question_data.get('difficulty', None)
+
+      question_text = body.get('question', None)
+      answer = body.get('answer', None)
+      category = body.get('category', None)
+      difficulty = body.get('difficulty', None)
 
       # if one of the values is None, abort the request as (BAD REQUEST)
       if ((question_text is None) or (answer is None) or 
@@ -219,22 +217,23 @@ def create_app(test_config=None):
   category to be shown. 
   '''
 
-  @app.route('/categories/<string:category_type>/questions')
-  def get_questions_by_category(category_type):
+
+  @app.route('/categories/<int:category_id>/questions')
+  def get_questions_by_category(category_id):
     
     # get all the questions by category
-    questions = Question.query.filter(Question.category == category_type).all()
+    questions = Question.query.filter(Question.category == str(category_id+1)).all()
 
     formatted_questions = pagination(request, questions)
 
     # abort with 404 if no questions by that category or he enter page that does not exist
     if len(formatted_questions) == 0:
       abort(404)
-
+    
     return jsonify({
       "success": True,
       "questions": formatted_questions,
-      "current_category": category_type,
+      "current_category": category_id,
       "total_questions": len(questions)
     })
   
@@ -252,36 +251,38 @@ def create_app(test_config=None):
 
   @app.route('/quizzes', methods=['POST'])
   def play_quiz():
-  
 
-    request_body = request.get_json()
+    body = request.get_json()
 
-    # getting the previous questions and the category
-    category_type = request_body.get('quiz_category')
-    previous_questions = request_body.get('previous_questions')
-
-    if (category_type is None) or (previous_questions is None):
+    if not ('quiz_category' in body and 'previous_questions' in body):
       # rise a BAD REQUEST, if one of the values are NONE
       abort(400)
     
-    # getting the all questions of the requested category
-    category_questions = Question.query.filter(Question.category == category_type).all()
+
+    # getting the previous questions and the category
+    category_type = body.get('quiz_category')['id']
+    previous_questions = body.get('previous_questions')
+    category_questions = []
+
+    if category_type == 0:
+      # get all category
+      category_questions = Question.query.all()
+    else:
+      # getting the all questions of the requested category
+      category_questions = Question.query.filter(Question.category == category_type).all()
 
     # abort if this category does not have questions
     if len(category_questions) == 0:
       abort(404)
-    
-    # take only the question text for easier camparison
-    questions_text = [question['question'] for question in previous_questions]
 
 
     def new_questions_filter(previous_questions, all_questions):
       # this method will filter the questions, and get new questions ONLY
       new_questions = []
-      for check_question in category_questions:
+      for check_question in all_questions:
         its_new = True
-        for used_question in questions_text:
-          if check_question.question == used_question:
+        for used_question_id in previous_questions:
+          if check_question.id == used_question_id:
             its_new = False
         
         if its_new:
@@ -294,7 +295,7 @@ def create_app(test_config=None):
       return new_questions[index]
 
 
-    new_questions = new_questions_filter(questions_text, category_questions)
+    new_questions = new_questions_filter(previous_questions, category_questions)
 
     if len(new_questions) == 0:
       # This mean he have used all the questions available
